7ad8b2aee94d47c0f0ed23b93ae7cd4b57179f3f
Felipe Lunkes <felipemiguel_nery@hotmail.com>
2022-09-05 11:49:25 -0300

    Classificação do IMC

--- IMCalc.py
+++ IMCalc.py
@@ -2,6 +2,7 @@
 
 # Calculadora de IMC, por Felipe Miguel Nery Lunkes
 
+from site import ENABLE_USER_SITE
 from tkinter import *
 
 class IMCalc:
@@ -38,6 +39,29 @@ class IMCalc:
         resultado=peso/((altura/100)*(altura/100))
 
         self.texto3.insert(END, str(resultado))
+
+        if float(resultado) <= 17:
+            classificacao="Muito abaixo do peso"
+        elif float(resultado) >= 17 and float(resultado) < 18.49:
+            classificacao="Abaixo do peso"
+        elif float(resultado) >= 18.50 and float(resultado) < 24.99:
+            classificacao="Peso normal"
+        elif float(resultado) >= 25 and float(resultado) <= 29.99:
+            classificacao="Acima do peso"
+        elif float(resultado) >= 30 and float(resultado) <= 34.99:
+            classificacao="Obesidade grau I"
+        elif float(resultado) >= 35 and float(resultado) <= 39.99:
+            classificacao="Obesidade grau II (severa)"
+        elif float(resultado) >= 40:
+            classificacao="Obesidade III (mórbida)"
+        else:
+            classificacao="Desconhecida"
+
+        lblCalssificacao=Label(janela, text='Classificação:')
+        lblCalssificacao.place(x=50, y=250)
+        classific=Entry()
+        classific.place(x=150, y=250)
+        classific.insert(END, str(classificacao))
 
 janela=Tk()
 imcalc=IMCalc(janela)
